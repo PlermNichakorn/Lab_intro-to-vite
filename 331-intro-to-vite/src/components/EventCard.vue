dac5c9998bd001296c076803fb1fc737e8767897
PlermNichakorn <nichakorn_prompong@cmu.ac.th>
2024-07-16 15:02:32 +0700

    Lab03_1.6

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -33,8 +33,8 @@ defineProps<{
   <RouterLink to="/event/123">
     <div class="event-class">
       <div class="event-card">
-        <span>@{{ event.time }} on {{ event.date }}</span>
-        <h4>{{ event.title }}</h4>
+        <span>@{{ event?.time }} on {{ event?.date }}</span>
+        <h4>{{ event?.title }}</h4>
       </div>
     </div>
   </RouterLink>
