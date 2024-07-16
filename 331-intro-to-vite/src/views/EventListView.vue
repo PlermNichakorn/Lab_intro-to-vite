feea579eb6acac5498eb3d75dbb0477cecde65f2
PlermNichakorn <nichakorn_prompong@cmu.ac.th>
2024-07-16 17:32:17 +0700

    Lab03_2.8

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -46,12 +46,16 @@ const hasNextPage = computed(() => {
   <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <RouterLink :to="{ name: 'event-list-view', query: { page: page - 1 } }" rel="prev" v-if="page!=1">
+    <div class="pagination">
+      <RouterLink :to="{ name: 'event-list-view', query: { page: page - 1 } }" rel="prev" v-if="page!=1" id="page-prev">
       Prev Page
     </RouterLink>
-    <RouterLink :to="{ name: 'event-list-view', query: { page: page + 1 } }" rel="prev" v-if="hasNextPage">
+
+    <RouterLink :to="{ name: 'event-list-view', query: { page: page + 1 } }" rel="prev" v-if="hasNextPage" id="page-next">
       Next Page
     </RouterLink>
+    </div>
+    
   </div>
 </template>
 
@@ -60,5 +64,23 @@ const hasNextPage = computed(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination{
+  display: flex;
+  width: 290px;
+}
+
+.pagination a{
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev{
+  text-align: left;
+}
+
+#page-next{
+  text-align: right;
 }
 </style>
